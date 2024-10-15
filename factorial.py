619ca3feabcbcd0e10dffc3182f30b83f3e0de7d
KsiuSallivan <ksiu.sallivan@gmail.com>
2024-10-15 18:13:56 +0300

    factorial.py

--- factorial.py
+++ factorial.py
@@ -6,11 +6,23 @@
 #     return f
 
 
-def factorial(num):
+def factorial_1(num):
     f = 1  # Начинаем с 1, так как факториал 0 равен 1
     for i in range(1, num + 1):
         f *= i  # Умножаем f на i для получения факториала
     return f
 
 
-print(factorial(5))
+print(factorial_1(5))
+
+
+def factorial_2(n):
+    # Базовый случай
+    if n == 0:
+        return 1
+    # Рекурсивный случай
+    else:
+        return n * factorial_2(n - 1)
+
+
+print(factorial_2(6))
